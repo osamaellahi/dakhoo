558890778743022a5c43487c4a6092a83655cc50
Osama ellahi <m.osamaellahi@gmail.com>
2020-09-08 15:27:20 +0500

    new

---
+++ resources/views/message/about.blade.php
@@ -0,0 +1,218 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
+<!-- Popper JS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+body{
+		height: 100%;
+		background-image: url("https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTQJVT9lwMNVswbT4OPKsg4WOGdaW8BXwpHQQ&usqp=CAU");
+	}
+	@media screen and (max-width: 600px) {
+					#heal{
+						font-size:8px;
+					}
+			}
+#heal{
+			font-size:20px;
+	}
+#me{
+	background:rgb(247, 241, 227);
+	padding:12px;
+	margin:1% 5% 0% 5%;
+	border-top:1px solid green;
+	border-left:1px solid green;
+	border-right:1px solid green;
+	overflow-y:scroll;
+	
+}
+
+/* width */
+::-webkit-scrollbar {
+  width: 10px;
+}
+
+/* Track */
+::-webkit-scrollbar-track {
+  background: #f1f1f1; 
+}
+ 
+/* Handle */
+::-webkit-scrollbar-thumb {
+  background: #8118; 
+}
+
+/* Handle on hover */
+::-webkit-scrollbar-thumb:hover {
+  background:black; 
+}
+body {
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+.flip-card {
+
+  background-color: transparent;
+  width: 300px;
+  height: 300px;
+  margin:15px;
+  perspective: 1000px;
+}
+
+.flip-card-inner {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  text-align: center;
+  transition: transform 0.6s;
+  transform-style: preserve-3d;
+  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+}
+
+.flip-card:hover .flip-card-inner {
+  transform: rotateY(180deg);
+}
+
+.flip-card-front, .flip-card-back {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  backface-visibility: hidden;
+}
+
+.flip-card-front {
+  background-color: #bbb;
+  color: black;
+}
+.flip-card-front img{
+	border:2px solid aqua;
+		border-radius:6px 6px 5px 6px;
+		}
+
+.flip-card-back {
+  background-color: #2980b9;
+  color: white;
+  transform: rotateY(180deg);
+}
+</style>
+</head>
+<body>
+<h2 style="text-align:center;color:#ccc" ><hr />Owners of the anonymous chat <a class="btn btn-success" href="./" style="padding:10px;float:right;margin-right:2%;">BACK</a><hr /></h2>
+<div class="container">
+<div class="flip-card" style="display:inline-block">
+  <div class="flip-card-inner">
+    <div class="flip-card-front">
+      <img src="https://media-exp1.licdn.com/dms/image/C4D03AQH4btnZ5LYhwQ/profile-displayphoto-shrink_400_400/0?e=1604534400&v=beta&t=yFv5RskN79lo4y8AG_qCJDc5EGmuUYR0-wyxWKmxuIQ" alt="Avatar" style="width:100%;height:100%;">
+    </div>
+    <div class="flip-card-back">
+      <h1>Nouman Ali</h1> 
+      <p>Computer Scientest</p> 
+      <p>Web engineer</p>
+      <h5>Calculus expert</h5>
+      <h6>Comsats university wah,cantt</h6>	  
+    </div>
+  </div>
+</div>
+<div class="flip-card"  style="display:inline-block">
+  <div class="flip-card-inner">
+    <div class="flip-card-front">
+      <img src="https://media-exp1.licdn.com/dms/image/C5103AQE1s8i-ZABO8w/profile-displayphoto-shrink_400_400/0?e=1604534400&v=beta&t=hJbidRRSDzoxpDl1CZN18QjeuI85EzR4mQ5JCD7UHXE" alt="Avatar" style="width:100%;height:100%;">
+    </div>
+    <div class="flip-card-back">
+       <h1>Bilal hassan</h1> 
+      <p>Computer Scientest</p> 
+      <p>Web engineer</p>
+      <p>Cybersecurity engineer</p>
+      <h5>Illustrator expert</h5>
+      <h6>Comsats university wah,cantt</h6>	 
+    </div>
+  </div>
+</div>
+<div class="flip-card"  style="display:inline-block">
+  <div class="flip-card-inner">
+    <div class="flip-card-front">
+      <img src="https://lh3.googleusercontent.com/owzftBJhOuSx17Iyl4Wd71sFWfynsuYVey5JrZbQ1X5BxAuJvoodRw3XqlOkh-y5y1vdePtqaD1WFFSK1KTtg_rfI1Ars3dBJAhShVYQB82fjHJshlePPrrk1gEAlcYjVZ6tRik7cDOaXkJAqBOyjZOLeW3ab5q_oJ_bl4_bJureat2ktrePReiVKX92C9Bbo8JowDiAXDcpsWVXoIDLxzd0VKptvWKRsIVXelhonx96_U-7A985l-c0B9QrJglL2zUVf4MmXGqjD-AvrgZZPkX685LKLTSdQvX3UGC_qIU2Gca40j28T38CA3rpM1VJbPXi4Xpz5q7B5CoN6SyO_LxSW4c6ON0HxEl1gFIfnqVvdAYtuRxsnDUk-8yHP5mBRNNUQP-5ciNAku3ywWcF61lx-u_kglDUlgqeJud_WOy_FnxHxAwHXESRMHJwPRkN3MwIvpCktQJDqeuZvKP7dr65PP3EAQ7zrICRqxMyHmTTUnJ3tLFR2X5a4-0i99ds1jbpe4Wetq36nKxY0r4JEoH6z14GZf4R9j7tROvW_xLpsHGitnE3I7sn0uU4os0JkBRE-D-1T-HZOmpzwYyAmdX6ciX07cbffryugbaRKFOr4-egq4UM8DYGKDlbscyohmv2qHq7V0qrjIU9KiyaxCMK0_jGXj-sp6hXxT-io-V1thzHuOpg1k1wIgKdqcqfxoCT4MsUJbi_sbt-ACfHKJ64sA=w387-h337-no" alt="Avatar" style="width:100%;height:100%;">
+    </div>
+    <div class="flip-card-back">
+      <h1>Muhammad Umer</h1> 
+      <p>Computer Scientest</p> 
+      <p>Web engineer</p>
+      <h5>Android expert</h5>
+      <h6>Comsats university wah,cantt</h6>	 
+    </div>
+  </div>
+</div>
+<div class="flip-card"  style="display:inline-block">
+  <div class="flip-card-inner">
+    <div class="flip-card-front">
+      <img src="https://media-exp1.licdn.com/dms/image/C4D03AQGlc5kqJnneJA/profile-displayphoto-shrink_400_400/0?e=1604534400&v=beta&t=PVSV2k_JCK-u-1Y7H87_YEu6-85KcH-5Sug5ftSOjhI" alt="Avatar" style="width:100%;height:100%;">
+    </div>
+    <div class="flip-card-back">
+        <h1>Zohaib Banaras</h1> 
+      <p>Computer Scientest</p> 
+      <p>Web engineer</p>
+      <h5>Blockchain expert</h5>
+      <h6>Comsats university wah,cantt</h6>	 
+    </div>
+  </div>
+</div>
+
+<div class="flip-card"  style="display:inline-block">
+  <div class="flip-card-inner">
+    <div class="flip-card-front">
+      <img src="https://scontent.fisb1-1.fna.fbcdn.net/v/t1.0-9/40020780_1078993855591561_7012075560067137536_o.jpg?_nc_cat=107&_nc_sid=8bfeb9&_nc_eui2=AeE44oG5UB7oTkE8xdSHiUtlbjq8g22zRKBuOryDbbNEoM11iwnLRwrbhgpGInIhfjiTXEV-aHkWV9emRIbXxIZP&_nc_ohc=cC6hRSJA67cAX89Ng5A&_nc_ht=scontent.fisb1-1.fna&oh=5799a248cbe36d4d3fbdd7749e116b20&oe=5F75E2F6" alt="Avatar" style="width:100%;height:100%;">
+    </div>
+    <div class="flip-card-back">
+        <h1>Asim Ali</h1> 
+      <p>Computer Scientest</p> 
+      <p>TicToc engineer</p>
+      <h5>Pundii expert</h5>
+      <h6>Comsats university wah,cantt</h6>	 
+    </div>
+  </div>
+</div>
+
+<div class="flip-card"  style="display:inline-block">
+  <div class="flip-card-inner">
+    <div class="flip-card-front">
+      <img height="100%" src="https://media-exp1.licdn.com/dms/image/C4D03AQF5qopAfKs0ow/profile-displayphoto-shrink_400_400/0?e=1604534400&v=beta&t=Blznh0YpvG-v6m2CJLUxoHjOHScpI-W1_5zaRzxX4VI">
+    </div>
+    <div class="flip-card-back">
+        <h1>Ahmed Raza</h1> 
+      <p>Computer Scientest</p> 
+      <p>Web engineer</p>
+      <p>Android expert</p>
+      <p>Cyber expert</p>
+      <h5>Block chain expert</h5>
+      <h6>Comsats university wah,cantt</h6>	 
+    </div>
+  </div>
+</div>
+<div class="flip-card"  style="display:inline-block">
+  <div class="flip-card-inner">
+    <div class="flip-card-front">
+      <img src="https://scontent.fisb1-1.fna.fbcdn.net/v/t1.0-9/43120743_2222852454653001_2536778444327354368_n.jpg?_nc_cat=110&_nc_sid=09cbfe&_nc_eui2=AeFdna9VOEvI2DWQFM2k-LKkC_fOUpl75GIL985SmXvkYqFekBrxq-bE5LCf_R6MkcnuA67wnN3TXXYj6pYpx1tG&_nc_ohc=a5ww73QgKSUAX99kzGs&_nc_oc=AQnUGZutrf2QSX5OcHae4l8ecVyJ0-1Nl49crM0NNdtKpu02W8g8hDryhcxAKuoD80g&_nc_ht=scontent.fisb1-1.fna&oh=0d25d749d765556e32392241a8b24538&oe=5F73CCD2" alt="Avatar" style="width:100%;height:100%;">
+    </div>
+    <div class="flip-card-back">
+        <h1>Abdul hannan</h1> 
+      <p>Computer Scientest</p> 
+      <p>Web engineer</p>
+      <p>Android expert</p>
+      <h5>ALL ROUNDER expert</h5>
+      <h6>Comsats university wah,cantt</h6>	 
+    </div>
+  </div>
+</div>
+</div>
+</body>
+</html>
